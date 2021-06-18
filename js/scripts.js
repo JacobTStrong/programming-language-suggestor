fb5732f1944472aec0979dd81250b18ceb8d0660
Jacob Strong <rjts2896@gmail.com>
2021-06-17 21:23:52 -0700

    add click functions to yesPlease, noThankYou, giveName, and noName buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-  $(".btn#yes").click(function() {
-    $(".inputName").show();
+  $(".button#yesPlease").click(function() {
+    $("#intro").fadeToggle();
+		$("#inputName").fadeToggle();
   });
 
-  $(".btn#giveName").click(function() {
-    const unserName = prompt("What is your name?");
+  $(".button#noThankYou").click(function() {
+    $("#denyGreg").fadeToggle();
   });
+
+  $(".button#giveName").click(function() {
+    const userName = prompt("What is your name?");
+  });
+
+  $(".button#noName").click(function() {
+    $("#anonymous").fadeToggle();
+    $(".nameBox").fadeToggle();
+  });
+
 });
