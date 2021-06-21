768189ef2c8dd8d848fa50a5c33b8881f70edb46
Jacob Strong <rjts2896@gmail.com>
2021-06-20 22:29:04 -0700

    add finished scripts for language selector project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  $("#submitForm").click(function(event) {
+    const techStuff = parseInt($("input:radio[name=techStuff]:checked").val());
+    const seasons = parseInt($("input:radio[name=seasons]:checked").val());
+    const hobbies = parseInt($("input:radio[name=hobbies]:checked").val());
+    const hogwarts = parseInt($("input:radio[name=hogwarts]:checked").val());
+    const getaway = parseInt($("input:radio[name=getaway]:checked").val());
+    const total = (techStuff + seasons + hobbies + hogwarts + getaway);
+
+    if (total <=10) {
+    $('#jScript').show();
+    } else if (total >=14) {
+      $('#python').show();
+    } else {
+      $('#swift').show();
+    }
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
   $(".button#yesPlease").click(function() {
     $("#intro").fadeToggle();
 		$("#setName").fadeToggle();
@@ -12,7 +32,7 @@ $(document).ready(function() {
     $("#submitName").fadeToggle();
   });
 
-  $(".button#submitName").click(function() {
+  $(".button#submitName").click(function() { 
     $("#setName").fadeToggle();
     $("#nameProvided").fadeToggle();
   });
@@ -40,10 +60,21 @@ $(document).ready(function() {
   $(".button#continueThree").click(function() {
     $("#beginQuiz").fadeToggle();
     $("#suggestorForm").fadeToggle();
+    $(".button#submitForm").fadeToggle();
   });
 
   $(".button#noName").click(function() {
     $("#anonymous").fadeToggle();
     $("#nameBox").fadeToggle();
   });
+
+  $(".button#submitForm").click(function() {
+    $("#suggestorForm").fadeToggle();
+    $(".button#refreshQuiz").fadeToggle();
+  });
+
+  $(".button#refreshQuiz").click(function() {
+    location.reload()
+  });
+
 });
